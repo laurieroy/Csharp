309596207e6510cd28fd0144f435dd4a8fe36d59
Laurette Roy <laurieroy.dev@gmail.com>
2017-09-19 18:33:28 -0700

    Moved Directory

---
+++ MyFirstChallenge/MyFirstChallenge/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void moneyTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void fortuneButton_Click(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            string money = moneyTextBox.Text;
+
+            string result = "At " + age + " " + "years old, I would have expected you to have more than " + money + " in your pocket!";
+
+            resultLabel.Text = result;
+
+        }
+    }
+}
