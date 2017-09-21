aa25754c0f16f5bc800e882ff8c971316d623c06
Laurette Roy <laurieroy.dev@gmail.com>
2017-09-20 17:46:44 -0700

    Second c# challenge, build a simple calculator with some formatting

---
+++ challengeSimpleCalculator/challengeSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace challengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValueTextBox.Text;
+            string secondValue = secondValueTextBox.Text;
+
+            double firstValue_dbl = double.Parse(firstValue);
+            double secondValue_dbl = double.Parse(secondValue);
+
+            double result_dbl = firstValue_dbl + secondValue_dbl;
+
+            resultLabel.Text = result_dbl.ToString();
+        }
+
+        protected void multButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValueTextBox.Text;
+            string secondValue = secondValueTextBox.Text;
+
+            double firstValue_dbl = double.Parse(firstValue);
+            double secondValue_dbl = double.Parse(secondValue);
+
+            double result_dbl = firstValue_dbl * secondValue_dbl;
+
+            resultLabel.Text = result_dbl.ToString();
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValueTextBox.Text;
+            string secondValue = secondValueTextBox.Text;
+
+            double firstValue_dbl = double.Parse(firstValue);
+            double secondValue_dbl = double.Parse(secondValue);
+
+            double result_dbl = firstValue_dbl - secondValue_dbl;
+
+            resultLabel.Text = result_dbl.ToString();
+
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            string firstValue = firstValueTextBox.Text;
+            string secondValue = secondValueTextBox.Text;
+
+            double firstValue_dbl = double.Parse(firstValue);
+            double secondValue_dbl = double.Parse(secondValue);
+
+            double result_dbl = firstValue_dbl / secondValue_dbl;
+
+            resultLabel.Text = result_dbl.ToString();
+        }
+
+        protected void firstValueTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
