2767c27a93461a309bf095a76aa68e3d171ed7a2
Laurette Roy <laurieroy.dev@gmail.com>
2017-09-27 19:17:46 -0700

    C# challenge, using input from a calendar with some logic. Note: There is no protection against selecting an earlier date.

---
+++ Challenge_EpicSpiesAssignment/Challenge_EpicSpiesAssignment/Default.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Challenge_EpicSpiesAssignment.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 242px;
+            height: 300px;
+        }
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h2>
+            <img id="logo" alt="Epic Spies Logo" class="auto-style1" height="190" src="epic-spies-logo.jpg" /></h2>
+        <h2><span class="auto-style2">Spy New Assignment Form</span></h2>
+        <br />
+        Spy Code Name:
+        <asp:TextBox ID="spyNameTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        New Assignment Name:
+        <asp:TextBox ID="newOpNameTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <p>
+            End Date of Previous Assignment:</p>
+        <p>
+            <asp:Calendar ID="lastAssCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Start Date of Next Assignment:</p>
+        <p>
+            <asp:Calendar ID="nextAssCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            Projected End Date of Next Assignment:</p>
+        <p>
+            <asp:Calendar ID="endAssCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Assign Spy" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Challenge_EpicSpiesAssignment/Challenge_EpicSpiesAssignment/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Challenge_EpicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // initialize first load values to today, assignment in 2 weeks, duration of 1 week
+            if (!Page.IsPostBack)
+            {
+                lastAssCalendar.SelectedDate = DateTime.Now.Date;
+                nextAssCalendar.SelectedDate = DateTime.Now.Date.AddDays(15);
+                nextAssCalendar.VisibleDate = DateTime.Now.Date.AddDays(15);
+                endAssCalendar.SelectedDate = DateTime.Now.Date.AddDays(22);
+                endAssCalendar.VisibleDate = DateTime.Now.Date.AddDays(22);
+            }
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            // declare vars
+            double union = 14;
+            double dblbudget = 0;
+            double dblassDays = 0;
+            lastAssCalendar.SelectedDate = DateTime.Now.Date;
+
+            DateTime dtlast = lastAssCalendar.SelectedDate; 
+            DateTime dtnext = nextAssCalendar.SelectedDate;
+            DateTime dtend = endAssCalendar.SelectedDate;
+
+            // check to be sure Union rules followed for next assignment time
+            // Daily rate is $500. If assigned > 21 days, $1000 bonus
+            TimeSpan tsduration = (dtnext.Subtract(dtlast));
+            double intervaldays = tsduration.Days;
+
+            if (intervaldays > union)
+            {
+                dblassDays = dtend.Subtract(dtnext).Days;
+                dblbudget = 500 * dblassDays;
+                dblbudget = (dblassDays > 21) ? dblbudget += 1000 : dblbudget;
+
+                resultLabel.Text = String.Format("Assignment of {1} to {2} is authorized. Budget Total: {0:C}",
+                dblbudget, spyNameTextBox.Text, newOpNameTextBox.Text);
+            }
+            else
+            {
+                resultLabel.Text = "Error: Must allow at least two weeks between previous assignment and new assignment. ";
+                DateTime reccomendStart = lastAssCalendar.SelectedDate.AddDays(15);
+                nextAssCalendar.SelectedDate = reccomendStart;
+                nextAssCalendar.VisibleDate = reccomendStart;
+            }
+
+        }
+    }
+}
