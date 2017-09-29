86ba224c3119bb1f55537e6ef5bdf0db5d46ff52
Laurette Roy <laurieroy.dev@gmail.com>
2017-09-28 19:43:58 -0700

    Updated with their soln commented out.

--- ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
+++ ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
@@ -16,14 +16,15 @@ namespace ChallengeForXmenBattleCount
 
             string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
             int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
-
+            // thier soln smallestNumberIndex ="";
+            //     largestNumberIndex ="";
             string result = "";
-            
+
             for (int i = 0; i < names.Length; i++)
             {
                 if (numbers[i] == numbers.Max())
                 {
-                   
+
                     result = String.Format("Most Battles belongs to : {0} (value: {1})<br />", names[i], numbers[i].ToString());
                 }
                 if (numbers[i] == numbers.Min())
@@ -31,9 +32,24 @@ namespace ChallengeForXmenBattleCount
                     result += String.Format("Least Battles belongs to : {0} (value: {1})", names[i], numbers[i].ToString());
                     break;
                 }
+                /* Their solution wrote it out which is probably what happens behind the scense with max and min
+                 if (numbers[i] > number[largestNumberIndex])
+                 {
+                    largestNumberIndex = i;
+                 }             
+                 if (numbers[i] < number[smallestNumberIndex])
+                 {
+                    smallestNumberIndex = i;
+                 }
+                 
+                
+                 
             }
-
-            resultLabel.Text = result;
+            result = String.Format("Most Battles belongs to : {0} (value: {1})<br />", names[largestNumberIndex], numbers[largestNumberIndex]);
+            result += String.Format("<br />Least Battles belongs to : {0} (value: {1})", names[smallestNumberIndex], numbers[smallestNumberIndex]);
+            */
+            }
+            resultLabel.Text = result; 
         }
     }
 }
