b091e23e6ff46c2e6f3dfe3b3a246f84b06eefda
Laurette Roy <laurieroy.dev@gmail.com>
2017-09-28 17:06:08 -0700

    First commit of Challenge to track elections rigged and acts of mayhem by agents.

---
+++ challengeEpicSpiesAssetTracker/challengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="challengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 242px;
+            height: 300px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <img alt="epic spies logo" class="auto-style1" height="150" src="epic-spies-logo.jpg" /><br />
+        <h1>Asset Performance Tracker</h1>
+        <br />
+        Asset Name:
+        <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Elections Rigged:
+        <asp:TextBox ID="riggedTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Acts of Subterfuge Performed:
+        <asp:TextBox ID="actsTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="Add Asset" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ challengeEpicSpiesAssetTracker/challengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace challengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        double [] acts;
+        double [] votes;
+        string [] names;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                names = new string[0];
+                votes = new double[0];
+                acts = new double[0];
+                ViewState.Add("Names", names);
+                ViewState.Add("Votes", votes);
+                ViewState.Add("Acts", acts);
+
+            }
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            string[] names = (string[])ViewState["Names"];
+            double[] votes = (double[])ViewState["Votes"];
+            double[] acts = (double[])ViewState["Acts"];
+
+            Array.Resize(ref names, names.Length + 1);  // this seems horribly inefficient
+            Array.Resize(ref acts, acts.Length + 1);
+            Array.Resize(ref votes, votes.Length + 1);
+
+            int newestItem = names.GetUpperBound(0); // Get highest index, and place values
+            names[newestItem] = nameTextBox.Text;
+            votes[newestItem] = int.Parse(riggedTextBox.Text);
+            acts[newestItem] = int.Parse(actsTextBox.Text);
+
+            ViewState["Names"] = names; // reset in memory
+            ViewState["Votes"] = votes;
+            ViewState["Acts"] = acts;
+       
+            resultLabel.Text = String.Format("Total elections rigged: {0}<br /> " +
+                           "Average Acts of Subterfuge per Asset: {1:N2}<br />(Last Asset you Added:{2})", +
+                           votes.Sum(), acts.Average(), names[newestItem]);
+                                
+        }
+    }
+}
